450ba5ffd91b520077b7634737d2a259128652d9
zslayer06 <zfrye06@gmail.com>
2017-06-23 14:28:30 -0600

    Added some C++ files, we need to look into overriding character movement in C++ instead of what is happening right now Dalton

---
+++ Source/Boulder/SideScrollerMovementComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "SideScrollerMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOULDER_API USideScrollerMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
